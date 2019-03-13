a9ad550b726086fadc08a067ee26a70bcd49b4fe
ARunningPig <liushangyou@126.com>
2019-03-13 14:01:12 +0800

    test

---
+++ hibernate4/src/main/java/tech/jichang/tutorials/hibernate4/App.java
@@ -0,0 +1,13 @@
+package tech.jichang.tutorials.hibernate4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
